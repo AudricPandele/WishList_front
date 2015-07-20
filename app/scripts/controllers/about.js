58dec77da85cf79d306a5726f967831a47f8e09f
Audric PANDELE <apandele@snapp.fr>
2015-07-20 14:53:25 +0200

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wishListApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wishListApp
+ */
+angular.module('wishListApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wishListApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wishListApp
+ */
+angular.module('wishListApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
